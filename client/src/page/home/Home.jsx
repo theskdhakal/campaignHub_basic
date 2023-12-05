31084a5e14917613a9ff1d8265209b7e63e8324a
Shiva Dhakal <theskdhakal@gmail.com>
2023-12-05 14:10:17 +1100

    homepage edit

--- client/src/page/home/Home.jsx
+++ client/src/page/home/Home.jsx
@@ -1,12 +1,14 @@
 import { MainLayout } from "../../component/layout/MainLayout";
 import Feed from "../../component/feed/Feed";
 import Form from "../../component/content-form/Form";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchContentAction } from "./contentAction";
 import { useEffect } from "react";
 
 export const Home = () => {
   const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.user);
 
   useEffect(() => {
     dispatch(fetchContentAction());
@@ -14,10 +16,16 @@ export const Home = () => {
 
   return (
     <MainLayout>
-      <Form />
-      <hr />
+      {user?._id ? (
+        <>
+          <Form />
+          <hr />
 
-      <Feed />
+          <Feed />
+        </>
+      ) : (
+        <Feed />
+      )}
     </MainLayout>
   );
 };
