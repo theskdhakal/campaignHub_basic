408b41e387763fc5708a0e0cb705cc503fef70c5
Shiva Dhakal <theskdhakal@gmail.com>
2023-12-14 11:36:38 +1100

    dashboard updated

--- client/src/page/user/Dashboard.js
+++ client/src/page/user/Dashboard.js
@@ -21,29 +21,35 @@ const Dashboard = () => {
       <h1 className="text-center mb-4 dashboard-title">Dashboard</h1>
 
       <hr />
-      {userPosts.map((item, i) => (
-        <article
-          key={i}
-          className="mx-auto w-4/5 md:w-1/2 lg:w-1/3 xl:w-4/5 mt-3 overflow-hidden rounded-lg shadow transition hover:shadow-lg"
-        >
-          <div className="bg-white p-4 sm:p-6">
-            <time dateTime="2022-10-10" className="block text-xs text-gray-500">
-              {item.createdAt}
-            </time>
+      {userPosts
+        ? userPosts.map((item, i) => (
+            <article
+              key={i}
+              className="mx-auto w-4/5 md:w-1/2 lg:w-1/3 xl:w-4/5 mt-3 overflow-hidden rounded-lg shadow transition hover:shadow-lg"
+            >
+              <div className="bg-white p-4 sm:p-6">
+                <time
+                  dateTime="2022-10-10"
+                  className="block text-xs text-gray-500"
+                >
+                  {item.createdAt}
+                </time>
 
-            <h3 className="mt-0.5 text-lg text-gray-900">{item.description}</h3>
+                <h3 className="mt-0.5 text-lg text-gray-900">
+                  {item.description}
+                </h3>
 
-            <div className="flex justify-center">
-              {" "}
-              <img
-                src={item.image}
-                alt="loading"
-                style={{ maxWidth: "100%", height: "auto" }}
-              />
-            </div>
-          </div>
-        </article>
-      ))}
+                <div className="flex justify-center">
+                  <img
+                    src={item.image}
+                    alt="loading"
+                    style={{ maxWidth: "100%", height: "auto" }}
+                  />
+                </div>
+              </div>
+            </article>
+          ))
+        : "abcd"}
     </MainLayout>
   );
 };
